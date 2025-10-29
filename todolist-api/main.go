15dc03054d2ee86a70fd58501971b37b61c6d1b8
ShoAnn <hikarinoaibou66@gmail.com>
2025-10-29 18:47:16 +0700

    update todolist-api: refactor main.go

--- todolist-api/main.go
+++ todolist-api/main.go
@@ -3,21 +3,46 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
+	type Task struct {
+		ID        string    `json:"id"`
+		Title     string    `json:"title"`
+		Completed bool      `json:"completed"`
+		CreatedAt time.Time `json:"createdAt"`
+		UpdatedAt time.Time `json:"updatedAt"`
+	}
+
 	mux.HandleFunc("GET /tasks", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Get all tasks")
+		// get all tasks code
 	})
+
 	mux.HandleFunc("GET /tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id := r.PathValue("id")
 		fmt.Fprintf(w, "Get a task with the id %s", id)
+		// get 1 task code
 	})
 
 	mux.HandleFunc("POST /tasks", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Post a task")
+		// create task code
+	})
+
+	mux.HandleFunc("PUT /tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Fprintf(w, "Update task with the id %s", id)
+		// update code
+	})
+
+	mux.HandleFunc("DELETE /tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Fprintf(w, "Delete task with the id %s", id)
+		// delete code
 	})
 
 	if err := http.ListenAndServe("localhost:8080", mux); err != nil {

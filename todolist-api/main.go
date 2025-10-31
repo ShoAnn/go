b1fcfb2e1b4d26fd445df2889a54c6d8469a0b00
ShoAnn <hikarinoaibou66@gmail.com>
2025-10-31 09:08:04 +0700

    update todolist-api: refactor main.go

--- todolist-api/main.go
+++ todolist-api/main.go
@@ -17,35 +17,37 @@ func main() {
 		UpdatedAt time.Time `json:"updatedAt"`
 	}
 
-	mux.HandleFunc("GET /tasks", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Get all tasks")
-		// get all tasks code
-	})
-
-	mux.HandleFunc("GET /tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		fmt.Fprintf(w, "Get a task with the id %s", id)
-		// get 1 task code
-	})
-
-	mux.HandleFunc("POST /tasks", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Post a task")
-		// create task code
-	})
-
-	mux.HandleFunc("PUT /tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		fmt.Fprintf(w, "Update task with the id %s", id)
-		// update code
-	})
-
-	mux.HandleFunc("DELETE /tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		fmt.Fprintf(w, "Delete task with the id %s", id)
-		// delete code
-	})
+	mux.HandleFunc("GET /tasks", getTasks)
+	mux.HandleFunc("GET /tasks/{id}", getTask)
+	mux.HandleFunc("POST /tasks", createTask)
+	mux.HandleFunc("PUT /tasks/{id}", updateTask)
+	mux.HandleFunc("DELETE /tasks/{id}", deleteTask)
 
 	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
 		fmt.Println(err.Error())
 	}
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Get all tasks")
+	// get all tasks code
+}
+func getTask(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	fmt.Fprintf(w, "Get a task with the id %s", id)
+	// get 1 task code
+}
+func createTask(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post a task")
+	// create task code
+}
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	fmt.Fprintf(w, "Update task with the id %s", id)
+	// update code
+}
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	fmt.Fprintf(w, "Delete task with the id %s", id)
+	// delete code
 }
